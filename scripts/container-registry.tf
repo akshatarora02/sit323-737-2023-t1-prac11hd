4977904301ad0209eff1ad398eed8cba44dedc57
akshatarora02 <akshatarora028@gmail.com>
2023-05-30 00:57:06 +1000

    fix artifact registry resource

--- scripts/container-registry.tf
+++ scripts/container-registry.tf
@@ -1,6 +1,8 @@
 resource "google_artifact_registry_repository" "container_registry" {
   repository_id = var.app_name
   location      =  var.location
+  format = "DOCKER"
+
 }
 
 output "repository_name" {
